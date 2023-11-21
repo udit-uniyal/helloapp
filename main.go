8389dab9410a1abe42f42bd14e25a57b83794912
salman-accuknox <86464007+salman-accuknox@users.noreply.github.com>
2023-11-21 23:57:30 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "WORLD\n")
 }
-
+func whoamiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You\n")
+}
 func main() {
 	http.HandleFunc("/api/hello", helloHandler)
-
+   	http.HandleFunc("/api/whoami", whoamiHandler)
 	// Start the server on port 8080
 	fmt.Println("Server is running on :8080")
 	http.ListenAndServe(":8080", nil)
